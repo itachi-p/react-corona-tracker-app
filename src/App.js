a8cf13ff570aab291d90bbd9748a95744d1e90eb
itachi-P <itachip38@gmail.com>
2023-09-06 01:31:27 +0900

    feat:Display Title component in browser
    App>TopPage>Title is incorporated and displayed in the browser

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import TopPage from "./pages/TopPage"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div>
+      <TopPage />
     </div>
   );
 }
