abe6bd6e5e3cb4d54f30a32ab1116bf6de9e1575
itachi-P <itachip38@gmail.com>
2023-09-07 02:27:15 +0900

    feat:Stores data retrieved from API in state

--- src/App.js
+++ src/App.js
@@ -6,15 +6,32 @@ import './App.css';
 // 共有データを持つのはTopPageでも良いが、今回は学習目的の為に最上位のAppコンポーネントで管理する
 function App() {
   const [country, setCountry] = useState("");
+  const [countryData, setCountryData] = useState({
+    date: "",
+    newConfirmed: "",
+    totalConfirmed: "",
+    newRecovered: "",
+    totalRecovered: "",
+  });
+
   // セレクトボックスで選択された国のデータを取得する関数
   const getCountryData = () => {
     // fetchでAPIを叩く。文字列にstate等のJSコードを含めるにはバッククォートを使い、${}で囲む
     fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
-      .then((res) => res.json()) // fetchでアクセスしたURLからのレスポンスをJSON形式に変換
-      .then((data) => console.log(data)); // JSON形式に変換されたdataをコンソールに表示
+      .then(res => res.json()) // fetchでアクセスしたURLからのレスポンスをJSON形式に変換
+      .then(data => {
+        setCountryData({ // stateを更新
+          date: data[data.length - 1].Date,
+          newConfirmed: data[data.length - 1].Confirmed - data[data.length - 2].Confirmed,
+          totalConfirmed: data[data.length - 1].Confirmed,
+          newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
+          totalRecovered: data[data.length - 1].Recovered,
+        });
+      })
   }
   return (
     <div>
+      {console.log(countryData)}
       <TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData}/>
     </div>
   );
