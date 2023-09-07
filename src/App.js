16584668632c4bc31f77669ef91f58d32a00d823
itachi-P <itachip38@gmail.com>
2023-09-08 06:09:39 +0900

    feat:Add loading screen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 
 // 共有データを持つのはTopPageでも良いが、今回は学習目的の為に最上位のAppコンポーネントで管理する
 function App() {
+  const [loading, setLoading] = useState(false);
   const [country, setCountry] = useState("");
   const [countryData, setCountryData] = useState({
     date: "",
@@ -20,6 +21,7 @@ function App() {
 
   // セレクトボックスで選択された国のデータを取得する関数
   const getCountryData = () => {
+    setLoading(true);
     fetch(`https://monotein-books.vercel.app/api/corona-tracker/country/${country}`)
       .then(res => res.json())
       .then(data => {
@@ -30,9 +32,9 @@ function App() {
           newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
           totalRecovered: data[data.length - 1].Recovered,
         });
+        setLoading(false);
       })
       .catch(err => alert("エラーが発生しました。ページをリロードして、もう一度トライしてください。"));
-
   }
 
   // useEffectを使い、WorldPageコンポーネントが表示された時、自動で全世界の感染データを取得し表示する
@@ -46,7 +48,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} countryData={countryData}/>}/>
+        <Route path="/" element={<TopPage countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} countryData={countryData} loading={loading} />}/>
         <Route path="/world" element={<WorldPage allCountriesData={allCountriesData} />} />
       </Routes>
     </BrowserRouter>
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -4,14 +4,14 @@ import Selector from "../components/Selector";
 import Results from "../components/Results";
 
 // propsの代わりに分割代入を使い、コードを簡潔に書き換え
-const TopPage = ({ countriesJson, setCountry, getCountryData, countryData}) => {
+const TopPage = ({ countriesJson, setCountry, getCountryData, countryData, loading}) => {
   return (
     <div className="top-page-container">
       <div>
         <Header />
         <Title />
         <Selector countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} />
-        <Results countryData={countryData} />
+        <Results countryData={countryData} loading={loading} />
       </div>
     </div>
   );
