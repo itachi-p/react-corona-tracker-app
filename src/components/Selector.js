a8d132dc906a7dd3e18e9c099bec2b44fb1563df
itachi-P <itachip38@gmail.com>
2023-09-08 06:45:28 +0900

    feat:Eliminate Get Data button

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -5,13 +5,11 @@ const Selector = ({ setCountry, countriesJson, getCountryData} ) => {
       {/* eはユーザの操作によって発生したeventに関係する情報を持つオブジェクト */}
       {/* console.log(e)とすればChromeの「検証」>Consleで中身を全て確認可能 */}
       <select onChange={(e) => setCountry(e.target.value)}>
-        <option>Select a country</option> {/* 初期値 */}
         {/* map関数でcountriesJsonの中身を順番に取り出し、optionタグのリストを作成 */}
         {countriesJson.map((country, index) =>
           <option key={index} value={country.Slug}>{country.Country}</option>
         )}
       </select>
-      <button onClick={getCountryData}>Get Data</button>
     </div>
   );
 };
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -4,13 +4,13 @@ import Selector from "../components/Selector";
 import Results from "../components/Results";
 
 // propsの代わりに分割代入を使い、コードを簡潔に書き換え
-const TopPage = ({ countriesJson, setCountry, getCountryData, countryData, loading}) => {
+const TopPage = ({ countriesJson, setCountry, countryData, loading}) => {
   return (
     <div className="top-page-container">
       <div>
         <Header />
         <Title />
-        <Selector countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} />
+        <Selector countriesJson={countriesJson} setCountry={setCountry} />
         <Results countryData={countryData} loading={loading} />
       </div>
     </div>
